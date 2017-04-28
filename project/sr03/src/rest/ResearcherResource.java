5d033418e5205f545d57a61b90f0b1bcdb464332
Whiax <ragnaarok@hotmail.fr>
2017-04-28 18:51:12 +0200

    Ajout d'un chercheur

--- project/sr03/src/rest/ResearcherResource.java
+++ project/sr03/src/rest/ResearcherResource.java
@@ -6,10 +6,9 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.persistence.PersistenceException;
+import javax.validation.ConstraintViolationException;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Request;
@@ -28,13 +27,39 @@ public class ResearcherResource {
     public Researcher getResearcher(@PathParam("username") String username) {
         SessionFactory factory = new Configuration().configure().buildSessionFactory();
         Session session = factory.openSession();
-        String hql = "from Researcher R where R.username = :username";
-        Query query = session.createQuery(hql);
+        Query query = session.createQuery( "from Researcher R where R.username = :username");
         query.setParameter("username", username);
-        System.out.print("Test");
         Researcher researcher = (Researcher)query.list().get(0);
         return researcher;
     }
 
+
+
+    /*
+        AJOUT D'UN CHERCHEUR
+        Prends en paramètre un JSON (exp: {"name": "JP","surname": "JL","username": "LP2","email": "ave@ave.ave","password": "admin"})
+        Envoyé par POST.
+        Renvoie true ou false selon le succès de l'opération d'ajout.
+     */
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/{add}")
+    public boolean addResearcher(Researcher rs)
+    {
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        Session session = factory.openSession();
+        session.beginTransaction();
+        session.save(rs);
+        try
+        {
+            session.getTransaction().commit();
+        }
+        catch(PersistenceException E)
+        {
+            return false;
+        }
+        return true;
+    }
 
 }
