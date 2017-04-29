b3f31bdd0052c088d47c4a4b3d6304849600cb6a
Tamaskan <tom.puech@gmail.com>
2017-04-29 16:24:09 +0200

    add auth system

---
+++ project/sr03/src/forms/ConnexionForm.java
@@ -0,0 +1,127 @@
+package forms;
+
+/**
+ * Created by tompu on 29/04/2017.
+ */
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import beans.Researcher;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+public final class ConnexionForm {
+    private static final String CHAMP_EMAIL  = "email";
+    private static final String CHAMP_PASS   = "motdepasse";
+
+    private String              resultat;
+    private Map<String, String> erreurs      = new HashMap<String, String>();
+
+    public String getResultat() {
+        return resultat;
+    }
+
+    public Map<String, String> getErreurs() {
+        return erreurs;
+    }
+
+    public Researcher connectResearcher( HttpServletRequest request ) {
+        /* Récupération des champs du formulaire */
+        String email = getValeurChamp( request, CHAMP_EMAIL );
+        String password = getValeurChamp( request, CHAMP_PASS );
+
+        Researcher researcher = new Researcher();
+
+        /* Validation du champ email. */
+        try {
+            validationEmail( email );
+        } catch ( Exception e ) {
+            setErreur( CHAMP_EMAIL, e.getMessage() );
+        }
+//        researcher.setEmail( email );
+
+        /* Validation du champ mot de passe. */
+        try {
+            validationMotDePasse( password );
+        } catch ( Exception e ) {
+            setErreur( CHAMP_PASS, e.getMessage() );
+        }
+//        researcher.setPassword( password );
+
+        //RECUPERER DES DONNEES
+        if ( erreurs.isEmpty() ) {
+            SessionFactory factory = new Configuration().configure().buildSessionFactory();
+            Session session = factory.openSession();
+            Query query = session.createQuery("from Researcher R where R.email = :email");
+            query.setParameter("email", email);
+            ArrayList<Researcher> researcherList = (ArrayList<Researcher>) query.list();
+
+            if (researcherList.size() == 0) {
+                setErreur(CHAMP_EMAIL, "Cette adresse email ne correspond à aucun utilisateur.");
+            } else {
+                researcher = researcherList.get(0);
+                String dbPassword = researcher.getPassword();
+                if (dbPassword != null && !password.equals(dbPassword)) {
+                    setErreur(CHAMP_PASS, "Mot de passe incorrect.");
+                }
+            }
+        }
+
+        /* Initialisation du résultat global de la validation. */
+        if ( erreurs.isEmpty() ) {
+            resultat = "Succès de la connexion.";
+        } else {
+            resultat = "Échec de la connexion.";
+        }
+
+        return researcher;
+    }
+
+    /**
+     * Valide l'adresse email saisie.
+     */
+    private void validationEmail( String email ) throws Exception {
+        if ( email != null && !email.matches( "([^.@]+)(\\.[^.@]+)*@([^.@]+\\.)+([^.@]+)" ) ) {
+            throw new Exception( "Merci de saisir une adresse mail valide." );
+        }
+    }
+
+    /**
+     * Valide le mot de passe saisi.
+     */
+    private void validationMotDePasse( String motDePasse ) throws Exception {
+        if ( motDePasse != null ) {
+            if ( motDePasse.length() < 3 ) {
+                throw new Exception( "Le mot de passe doit contenir au moins 3 caractères." );
+            }
+        } else {
+            throw new Exception( "Merci de saisir votre mot de passe." );
+        }
+    }
+
+    /*
+     * Ajoute un message correspondant au champ spécifié à la map des erreurs.
+     */
+    private void setErreur( String champ, String message ) {
+        erreurs.put( champ, message );
+    }
+
+    /*
+     * Méthode utilitaire qui retourne null si un champ est vide, et son contenu
+     * sinon.
+     */
+    private static String getValeurChamp( HttpServletRequest request, String nomChamp ) {
+        String valeur = request.getParameter( nomChamp );
+        if ( valeur == null || valeur.trim().length() == 0 ) {
+            return null;
+        } else {
+            return valeur;
+        }
+    }
+}
