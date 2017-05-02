a80dd1d6375edde96f631ba5b4ca75bea73b15ef
clarktom <tom.puech@gmail.com>
2017-05-02 17:02:30 +0200

    add links to post

--- project/sr03/src/resources/PostResource.java
+++ project/sr03/src/resources/PostResource.java
@@ -18,8 +18,13 @@ public class PostResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Post> getAllPosts(@PathParam("topicId") Integer topicId) {
-        return postService.getAllPosts(topicId);
+    public List<Post> getAllPosts(@PathParam("topicId") Integer topicId, @Context UriInfo uriInfo) {
+        List<Post> posts = postService.getAllPosts(topicId);
+        for (Post post : posts) {
+            post.addLink(getUriForSelf(uriInfo, post), "self");
+            post.addLink(getUriForResearcher(uriInfo, post), "researcher");
+        }
+        return posts;
     }
 
     @GET
@@ -29,9 +34,29 @@ public class PostResource {
                         @PathParam("postId") Integer postId,
                         @Context UriInfo uriInfo) {
         Post post = postService.getPost(topicId, postId, uriInfo);
-//        post.addLink(getUriForSelf(id, uriInfo), "self");
-//        post.addLink(getUriForStep(id, uriInfo), "self");
+        post.addLink(getUriForSelf(uriInfo, post), "self");
+        post.addLink(getUriForResearcher(uriInfo, post), "researcher");
         return post;
+    }
+
+    private String getUriForSelf(UriInfo uriInfo, Post post) {
+        return uriInfo.getBaseUriBuilder()
+                .path(IdeaResource.class)
+                .path(IdeaResource.class, "getTopicResource")
+                .resolveTemplate("ideaId", post.getTopic().getStep().getIdea().getIdeaId())
+                .path(TopicResource.class, "getPostResource")
+                .resolveTemplate("topicId", post.getTopic().getTopicId())
+                .path(Integer.toString(post.getPostId()))
+                .build()
+                .toString();
+    }
+
+    private String getUriForResearcher(UriInfo uriInfo, Post post) {
+        return uriInfo.getBaseUriBuilder()
+                .path(ResearcherResource.class)
+                .path(post.getResearcher().getUsername())
+                .build()
+                .toString();
     }
 
 //    private String getUriForStep(Integer id, UriInfo uriInfo) {
