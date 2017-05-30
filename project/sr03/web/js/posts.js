7fbce5c89f544bc7ca5b3624ee3a408967aac4a9
clarktom <tom.puech@gmail.com>
2017-05-30 18:50:47 +0200

    add posts.jsp

--- project/sr03/web/js/posts.js
+++ project/sr03/web/js/posts.js
@@ -2,21 +2,23 @@
  * Created by tompu on 30/05/2017.
  */
 
-function loadPosts() {
-    var articlesElt = document.getElementById("posts");
-    ajaxGet("http://localhost:8080/sr03/api/ideas", function (reponse) {
-        // Transforme la réponse en tableau d'objets JavaScript
-        var ideas = JSON.parse(reponse);
-        // Affiche le titre de chaque film
-        ideas.forEach(function (idea) {
-            console.log(idea.title);
-            // Ajout du titre et du contenu de chaque article
+
+function loadPosts(idIdea, idTopic)
+{
+    var div = document.getElementById("steps");
+    ajaxGet("http://localhost:8080/sr03/api/ideas/" + idIdea + "/topics/" + idTopic, function (reponse) {
+        var posts = JSON.parse(reponse);
+        console.log(steps);
+        posts.forEach(function (post) {
+
             var titreElt = document.createElement("h2");
-            titreElt.textContent = idea.title;
+            titreElt.textContent = post.postId;
+
             var contenuElt = document.createElement("p");
-            contenuElt.textContent = idea.description;
-            articlesElt.appendChild(titreElt);
-            articlesElt.appendChild(contenuElt);
+            contenuElt.textContent = post.text;
+
+            div.appendChild(titreElt);
+            div.appendChild(contenuElt);
         })
     });
 }
---
+++ project/sr03/web/posts.jsp
@@ -0,0 +1,14 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<html>
+    <head>
+        <jsp:include page="WEB-INF/head.jsp" />
+    </head>
+
+    <body onload="loadPosts(${param.ideaID}, ${param.topicId})">
+        <jsp:include page="WEB-INF/header.jsp" />
+        <div id="posts"></div>
+    </body>
+</html>
+
